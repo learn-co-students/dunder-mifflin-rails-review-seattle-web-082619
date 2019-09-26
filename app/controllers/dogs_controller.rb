8302bedf6205d455a7780ef908a6c1f650f5fb75
Krista Loven <krista.loven.87@gmail.com>
2019-09-26 10:50:41 -0700

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,46 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to @dog
+    else
+      render :new
+    end
+  end
+
+  def show
+    @employees = @dog.employees
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to @dog
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog.destroy
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,51 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :find_dogs, only: [:new, :edit, :update, :create]
+
+  def index 
+    @employees = Employee.all 
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+  def show
+    @dog = @employee.dog
+  end
+
+  def update
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @employee.destroy
+  end
+
+  private
+
+  def find_dogs
+    @dog = Dog.all
+  end
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :title, :alias, :dog_id, :office, :img_url)
+  end
 end
